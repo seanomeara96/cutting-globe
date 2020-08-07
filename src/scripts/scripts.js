0703b911cdbc4adced43f1cb16583af5536ae024
Sean <seanom96@gmail.com>
2020-08-07 12:39:29 +0100

    update

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,1 +1,10 @@
 import "../styles/styles.css";
+let slideshow;
+if (window.location.pathname === "/about.html") {
+  import("./modules/SlideShow").then((x) => {
+    slideshow = new x.default(5000);
+  });
+}
+if (module.hot) {
+  module.hot.accept();
+}
