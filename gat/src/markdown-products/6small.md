284578a482871c61041841799a1511ac67123b0a
Sean <seanom96@gmail.com>
2020-08-15 09:01:17 +0100

    update to frontmatter

--- gat/src/markdown-products/6small.md
+++ gat/src/markdown-products/6small.md
@@ -1,7 +1,9 @@
 ---
 slug: "/shop/6-x-small-cutting-globes"
 date: "2020-08-14"
-title: "6-x-small-cutting-globes"
+title: "6 x small cutting globes"
+price: "5.00"
+previewImage: "../images/small/1.png"
 ---
 
-stuff goes here
+Ideal for climbers ,slim stemmed plants ,herbs ,indoor plants ,peppers ,tomatoes etc comes with 6 closing pints to stop the new roots from opening.. the cutting globe.. Available on EBAY AND AMAZON videos on youtube google cutting globe visit our main website [www.cuttingglobe.com](https://www.cuttingglobe.com) for more details As seen on show me your garden SKY TV
--- gat/src/templates/product-template.js
+++ gat/src/templates/product-template.js
@@ -7,16 +7,63 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <main class="page-section">
+      <div class="wrapper">
+        <div class="rows">
+          <div class="rows__62">
+            <h1 class="headline">{frontmatter.title}</h1>
+            <span>€{frontmatter.price}</span>
+            <br />
+            <span>Tax Included</span>
+            <br />
+            <div class="commerce-buttons">
+              <button class="ui primary basic button">Add to Cart</button>
+              <br />
+              <button class="ui green button">Buy it now</button>
+            </div>
+            <div
+              class="description"
+              dangerouslySetInnerHTML={{ __html: html }}
+            ></div>
+          </div>
+          <div class="rows__38">
+            <div class="product-image">
+              <img src={frontmatter.previewImage} alt="preview image" />
+            </div>
+            <div class="product-image__preview-area">
+              <div class="product-image__preview-area__preview">
+                <img
+                  class="product-image__preview-area__preview-img"
+                  src="./images/small/2.png"
+                  alt=""
+                />
+              </div>
+              <div class="product-image__preview-area__preview">
+                <img
+                  class="product-image__preview-area__preview-img"
+                  src="./images/small/3.png"
+                  alt=""
+                />
+              </div>
+              <div class="product-image__preview-area__preview">
+                <img
+                  class="product-image__preview-area__preview-img"
+                  src="./images/small/4.png"
+                  alt=""
+                />
+              </div>
+              <div class="product-image__preview-area__preview">
+                <img
+                  class="product-image__preview-area__preview-img"
+                  src="./images/small/5.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
@@ -28,6 +75,8 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         slug
         title
+        price
+        previewImage
       }
     }
   }
